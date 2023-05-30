7d92f3e181a4fc7c91bc436936b0f599d736dbc5
golderboy <golderboy@gmail.com>
2023-05-30 23:48:09 +0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -4,5 +4,5 @@ set PYTHON=C:\Python\Python3106\python.exe
 set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
 set COMMANDLINE_ARGS=--precision full --no-half --xformers --autolaunch --skip-torch-cuda-test
-git pull
+
 call webui.bat
