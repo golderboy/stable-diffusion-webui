c8aa866018ba8a65c357179e6034f68480dab6b7
golderboy <golderboy@gmail.com>
2023-05-30 13:22:58 +0700

    Update webui-user.bat
    30052566

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON=C:\Python\Python311\python.exe
+set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --autolaunch
 
 call webui.bat
