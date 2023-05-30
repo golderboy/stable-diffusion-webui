3d233a32b26e889ae002ece8d5a7f429a3d0e578
golderboy <golderboy@gmail.com>
2023-05-30 13:28:23 +0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PYTHON=C:\Python\Python311\python.exe
+set PYTHON=C:\Python\Python3106\python.exe
 set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
 set COMMANDLINE_ARGS=--xformers --autolaunch
