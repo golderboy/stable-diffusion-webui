87829405cdeda672a7aa5addddd6c18c5da9097c
golderboy <golderboy@gmail.com>
2023-05-30 15:01:54 +0700

    Update webui-user.bat
    update for amd

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON=C:\Python\Python3106\python.exe
+set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--precision full --no-half --xformers --autolaunch --skip-torch-cuda-test
+git pull
 call webui.bat
