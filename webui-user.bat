925f51d172f1bd6805f3b8584ed9cb824b60ccff
golderboy <golderboy@gmail.com>
2023-05-31 00:12:25 +0700

    update amd

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON=C:\Python\Python3106\python.exe
+set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--opt-sub-quad-attention --lowvram --disable-nan-check --precision full --no-half --xformers --autolaunch --skip-torch-cuda-test
 
 call webui.bat
