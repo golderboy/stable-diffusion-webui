03a2629b8318220d02eea70894ce9b4bfb346f08
golderboy <golderboy@gmail.com>
2023-05-30 14:18:19 +0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=C:\Python\Python3106\python.exe
 set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --autolaunch --skip-torch-cuda-test
+set COMMANDLINE_ARGS=--xformers --autolaunch --skip-torch-cuda-test --prefer-binary
 
 call webui.bat
