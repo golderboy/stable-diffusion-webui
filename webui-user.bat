21f3e6b233471a9d85e232bd9ae26d2e301e38f6
golderboy <golderboy@gmail.com>
2023-05-31 15:15:45 +0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=C:\Python\Python3106\python.exe
 set GIT=C:\Users\golde\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
-set COMMANDLINE_ARGS=--opt-sub-quad-attention --lowvram --disable-nan-check --precision full --no-half --xformers --autolaunch --skip-torch-cuda-test
+set COMMANDLINE_ARGS=--opt-sub-quad-attention --lowvram --disable-nan-check --precision full --no-half --xformers --autolaunch --skip-torch-cuda-test --deepdanbooru --no-half --share --disable-safe-unpickle 
 
 call webui.bat
